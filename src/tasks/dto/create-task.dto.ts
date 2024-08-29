22a8d76c89a257a67aa149b70f3237bf0582886d
Eddu <azabacheeduardo85@gmail.com>
2024-08-28 21:49:04 -0500

    refactor: Update task DTOs to use TaskStatus enum 🤝

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,3 +1,6 @@
+import { TaskStatus } from "../constants/status";
+
 export class CreateTaskDto {
     description: string;
+    status?: TaskStatus
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,5 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { TaskStatus } from "../constants/status";
 import { CreateTaskDto } from "./create-task.dto";
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-    status: TaskStatus;
-}
+}       
